9e5dc4f9214104857175dbe4ead9241e88224b73
Jason Shimkoski <jshimkoski@me.com>
2020-04-25 14:41:36 -0400

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# clean build directories
-rm -rf build/
+# clean dist directory
 rm -rf dist/
 
+# build docs
+PURGE_CSS=true npx vue-cli-service build --dest dist/docs
+
+# build non-minified css
+npx parcel build src/styles/index.css --no-minify -d dist/css
+
 # build minified css
-npx parcel build src/styles/index.css -d build/css
+npx parcel build src/styles/index.css --out-file index.min.css -d dist/css
 
 # build web components
-npx parcel build src/wc/index.js -d build/wc
-
-# build docs
-PURGE_CSS=true npx vue-cli-service build
+npx parcel build src/wc/index.js -d dist/wc
