3169407933f68cd01bc80e26f9a4e68040c2d8e3
Jason Shimkoski <jshimkoski@me.com>
2020-04-26 22:29:54 -0400

    build now generates entire desired package

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+start=$SECONDS
+
 # clean dist directory
 rm -rf dist/
 
 # build docs
 PURGE_CSS=true npx vue-cli-service build --dest dist/docs
 
-# build all
+# build all web components and css
 npx parcel build simple-design-system/index.js
 
 # build non-minified css
@@ -15,5 +17,16 @@ npx parcel build simple-design-system/css/index.css --no-minify -d dist/css
 # build minified css
 npx parcel build simple-design-system/css/index.css --out-file index.min.css -d dist/css
 
-# build all web components
-npx parcel build simple-design-system/wc/index.js -d dist/wc
+for d in simple-design-system/wc/* ; do
+  if [[ "$d" == *"index.js"* ]]; then
+    # build all web components
+    npx parcel build $d -d dist/wc
+  else
+    # build individual web components
+    npx parcel build $d/index.js -d dist/wc/${d##*/}
+  fi
+done
+
+duration=$(( SECONDS - start ))
+
+printf "\nFinished build in $duration seconds.\n\n"
