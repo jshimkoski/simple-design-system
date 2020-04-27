1ee84e3535f9e3d56e69c8baf98ebcd3854b18c8
Jason Shimkoski <jshimkoski@me.com>
2020-04-27 19:53:25 -0400

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-read -p 'Version (major, minor, or patch): ' version
+# commit confirm prompt
+read -p "Have you committed all changes? (y,n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 0
+fi
+
+# are you sure confirm prompt
+read -p "Are you sure you want to continue? (y,n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 0
+fi
+
+# version type confirm prompt
+read -p 'Enter a version type (major, minor, or patch): ' version
 
 if [ "$version" == "major" ] || [ "$version" == "minor" ] || [ "$version" == "patch" ] ;then
-    echo Proceed with $version...
+    echo Proceeding with $version release...
     npm run build
-    # npm version $version
-    # git push —-tags origin master
-    # npm publish
+    npm version $version
+    git push —-tags origin master
+    npm publish
 else
-    printf "\nTry again. Use a version type for this release: major, minor, or patch.\n\n"
+    printf "\nCould not process release.\nEnsure to use a version type for this release: major, minor, or patch.\n\n"
+    exit 0
 fi
