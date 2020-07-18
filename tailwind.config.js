865d8e266dea0e7027a6f227e576a2574c3c218f
Jason Shimkoski <jshimkoski@me.com>
2020-07-17 22:25:47 -0400

    fix: fix colors of typography plugin, fix specificity

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,66 @@ const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   purge: false,
+  important: true,
   theme: {
+    typography: {
+      default: {
+        css: {
+          color: "var(--color-fgcolor)",
+          '[class~="lead"]': {
+            color: "var(--color-gray-700)",
+          },
+          a: {
+            color: "var(--color-gray-900)",
+          },
+          strong: {
+            color: "var(--color-gray-900)",
+          },
+          "ol > li::before": {
+            color: "var(--color-gray-600)",
+          },
+          "ul > li::before": {
+            backgroundColor: "var(--color-gray-400)",
+          },
+          hr: {
+            borderColor: "var(--color-gray-300)",
+          },
+          blockquote: {
+            color: "var(--color-gray-900)",
+            borderLeftColor: "var(--color-gray-300)",
+          },
+          h1: {
+            color: "var(--color-gray-900)",
+          },
+          h2: {
+            color: "var(--color-gray-900)",
+          },
+          h3: {
+            color: "var(--color-gray-900)",
+          },
+          h4: {
+            color: "var(--color-gray-900)",
+          },
+          "figure figcaption": {
+            color: "var(--color-gray-600)",
+          },
+          code: {
+            color: "var(--color-gray-900)",
+          },
+          pre: {
+            color: "var(--color-gray-700)",
+            backgroundColor: "var(--color-gray-100)",
+          },
+          thead: {
+            color: "var(--color-gray-900)",
+            borderBottomColor: "var(--color-gray-400)",
+          },
+          "tbody tr": {
+            borderBottomColor: "var(--color-gray-300)",
+          },
+        },
+      },
+    },
     colors: {
       // Static colors
 
