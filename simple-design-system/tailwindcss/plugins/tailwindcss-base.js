d63734b15f6a2db7b85920bde4e2a9d8fa24eb95
Jason Shimkoski <jshimkoski@me.com>
2020-07-26 14:11:45 -0400

    feat: add prefers-color-scheme checking to dark mode plugin, fix fontFamily in base

--- simple-design-system/tailwindcss/plugins/tailwindcss-base.js
+++ simple-design-system/tailwindcss/plugins/tailwindcss-base.js
@@ -10,7 +10,7 @@ module.exports = plugin(function ({ addBase }) {
     },
 
     "html, body, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, blockquote, pre, a, abbr, acronym, address, big, button, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, select, textarea, input, optgroup, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, main, menu, nav, output, progress, ruby, section, summary, time, mark, audio, video": {
-      fontSamily: "inherit",
+      fontFamily: "inherit",
       fontSize: "inherit",
       fontWeight: "inherit",
       color: "inherit",
--- simple-design-system/tailwindcss/plugins/tailwindcss-dark-mode.js
+++ simple-design-system/tailwindcss/plugins/tailwindcss-dark-mode.js
@@ -1,9 +1,26 @@
 const plugin = require("tailwindcss/plugin");
 
-module.exports = plugin(function ({ addVariant, e }) {
-  addVariant("dark", ({ modifySelectors, separator }) => {
-    modifySelectors(({ className }) => {
-      return `.dark .${e(`dark${separator}${className}`)}`;
+module.exports = plugin(function ({ addVariant, e, postcss }) {
+  // this variant handles both
+  // prefers-color-mode: dark
+  // as well all having a .dark
+  // class on the :root
+  addVariant("dark", ({ container, separator }) => {
+    const prefersColorSchemeRule = postcss.atRule({
+      name: "media",
+      params: "(prefers-color-scheme: dark)",
+    });
+    prefersColorSchemeRule.append(container.nodes);
+    container.append(prefersColorSchemeRule);
+    prefersColorSchemeRule.walkRules((rule) => {
+      const clone = rule.clone();
+      clone.selector = `:root.dark .${e(
+        `dark${separator}${rule.selector.slice(1)}`
+      )}`;
+      rule.selector = `:root:not(.light) .${e(
+        `dark${separator}${rule.selector.slice(1)}`
+      )}`;
+      container.append(clone);
     });
   });
 });
